45d8259754435f7f25e9e5fbd0ccd5080fc23457
ingryd <ingryddasilva7@gmail.com>
2024-10-01 22:58:05 -0300

    atualizando a estrutura de pastas das pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import QuemSomos from "./pages/QuemSomos";
-import DoeAgora from "./pages/DoeAgora";
+import Home from "./pages/Home/Home";
+import QuemSomos from "./pages/QuemSomos/QuemSomos";
+import DoeAgora from "./pages/DoeAgora/DoeAgora";
 import Header from "./components/Header";
 
 function App() {
